518b1ecbb4d1f821b19c0edb12038c7377ce47af
machine.steam@yandex.ru <machine.steam@yandex.ru>
2020-10-01 23:52:53 +0300

    Добавьте файлы проекта.

---
+++ Razor/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for one person",
+                Category = "Watersport",
+                Price = 275M,
+
+            };
+            Product myProduct1 = new Product
+            {
+                ProductID = 2,
+                Name = "Giga",
+                Description = "A video game",
+                Category = "Games",
+                Price = 17M
+            };
+            return View(myProduct);
+        }
+    }
+}
